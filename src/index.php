04c2432a4e9c0363546284f15a60c2f2950f0e1c
Volker Janz <volker.janz@innogames.com>
2024-03-24 15:22:01 +0100

    Use filemtime for style and script versions to update cache on changes

--- src/index.php
+++ src/index.php
@@ -10,6 +10,7 @@
 
 // global variables for your plugin
 define("MY_PLUGIN_PATH", plugin_dir_path(__FILE__));
+define("MY_PLUGIN_URL", plugin_dir_url(__FILE__));
 define("MY_PLUGIN_SLUG", "my-plugin");
 
 require_once MY_PLUGIN_PATH . "admin.php";
@@ -21,22 +22,32 @@ function setup_db() {
     // database setup code
 }
 
+function get_url($file) {
+    return MY_PLUGIN_URL . $file;
+}
+
+function get_path($file) {
+    return MY_PLUGIN_PATH . $file;
+}
+
 function enqueue_my_styles() {
-    $src = plugin_dir_url(__FILE__) . "style.css";
-    wp_enqueue_style("my-style", $src, array());
+    wp_enqueue_style("my-style", get_url("style.css"), array(), filemtime(get_path("style.css")));
 }
 
 function enqueue_my_scripts() {
-    $src = plugin_dir_url(__FILE__) . "script.js";
-    wp_enqueue_script("my-script", $src, array("jquery"));
+    wp_enqueue_script("my-script", get_url("script.js"), array("jquery"), filemtime(get_path("script.js")));
 }
 
-function enqueue_my_block() {
-    $src = plugin_dir_url(__FILE__) . "block.js";
-    wp_enqueue_script("my-block", $src, array("wp-blocks", "wp-element", "wp-editor"));
+function enqueue_my_blocks() {
+    wp_enqueue_script(
+        "my-block",
+        get_url("block.js"),
+        array("wp-blocks", "wp-element", "wp-editor"),
+        filemtime(get_path("block.js"))
+    );
 }
 
 add_action("wp_enqueue_scripts", "enqueue_my_styles");
 add_action("wp_enqueue_scripts", "enqueue_my_scripts");
 
-add_action("enqueue_block_editor_assets", "enqueue_my_block");
+add_action("enqueue_block_editor_assets", "enqueue_my_blocks");
